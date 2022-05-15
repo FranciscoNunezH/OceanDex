80985a53db08eee6944719c94b28658ae894b2f8
JosAnGoCa <josetono612@gmail.com>
2022-05-15 10:56:40 -0500

    cnhange: cambie la vista inicial de le mapa

--- FrontEnd/scripts/js_logic.js
+++ FrontEnd/scripts/js_logic.js
@@ -1,6 +1,10 @@
 // initialize the map on the "map" div with a given center and zoom
-var map = L.map('map').setView([20.674089, -105.264793], 13);
+var map = L.map("map").setView([23.446657, -102.007883], 5);
 
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer(
+    "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
+    {
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+).addTo(map);
