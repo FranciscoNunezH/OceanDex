0862411bfef4ad7ee49909e8893e3300f817e266
Francisco N <86805363+FranciscoNunezH@users.noreply.github.com>
2022-05-14 23:54:23 -0500

    Add files via upload

---
+++ FrontEnd/scripts/js_logic.js
@@ -0,0 +1,6 @@
+// initialize the map on the "map" div with a given center and zoom
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
