e5ce7f1ef847dda98823c96cc7ce65ec0eeacbb8
JosAnGoCa <josetono612@gmail.com>
2022-06-17 20:50:42 -0500

    add: nombre comun en las cartas del inicio

--- Frontend/scripts/script.js
+++ Frontend/scripts/script.js
@@ -8,18 +8,22 @@ fetch("https://ocean-dex-api.herokuapp.com/api/consultar-nombres")
     })
     .then(data => {
         spinner.remove();
+
         datos = data.map(especie => {
             const card = userCardTemplate.content.cloneNode(true).children[0];
             const img = card.querySelector("[data-card-img_img]");
             const link = card.querySelector("[data-card-link]");
             const text = card.querySelector("[data-card-text]");
+            const textC = card.querySelector("[data-card-textC]");
             const attr = document.createAttribute("href");
             attr.value = `./info.html?nombre=${especie.Nombre_cientifico}`;
             card.setAttributeNode(attr);
             text.textContent = especie.Nombre_cientifico;
+            textC.textContent = especie.Nombres_comunes;
             especiesContainer.append(card);
             return {
                 nombre: especie.Nombre_cientifico.toLowerCase(),
+                nombre_comun: especie.Nombres_comunes.toLowerCase(),
                 element: card,
             };
         });
@@ -30,7 +34,9 @@ const search_input = document.querySelector(".input-filtro");
 search_input.addEventListener("input", e => {
     const value = e.target.value.toLowerCase();
     datos.forEach(especie => {
-        const isVisible = especie.nombre.includes(value);
+        const isVisible =
+            especie.nombre.includes(value) ||
+            especie.nombre_comun.includes(value);
         especie.element.classList.toggle("hidden", !isVisible);
     });
 });
